af5d8e9eefc76dd9fe16f8d5ae86563ace4c94a5
Gérard Dethier <gerard@logion.network>
2022-07-08 08:50:42 +0200

    chore: remove useless branch.

--- packages/client/src/Error.ts
+++ packages/client/src/Error.ts
@@ -1,8 +1,6 @@
 export function newBackendError(error: any): Error {
     if(error.response.data.errorMessage) {
         return new Error(`${error.message}: ${error.response.data.errorMessage}`);
-    } else if(error.response.data.error) {
-        return new Error(`${error.message}: ${error.response.data.error}`);
     } else {
         return new Error(`${error.message}: ${error.response.data}`);
     }
