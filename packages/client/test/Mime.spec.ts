d076335d4ee8010b099315928da9ba165a8f9f08
Gérard Dethier <gerard@logion.network>
2022-10-12 15:59:17 +0200

    feat: MimeType now exposes extensions.
    logion-network/logion-internal#613

--- packages/client/test/Mime.spec.ts
+++ packages/client/test/Mime.spec.ts
@@ -6,16 +6,32 @@ describe("MimeType", () => {
         expect(() => MimeType.from("unknown/mime-type")).toThrow();
     });
 
-    it("creates known MIME type without charset", () => {
-        testValidMimeType("text/plain");
+    it("creates known MIME type without charset", () => testValidMimeType("text/plain"));
+    it("creates known MIME type with charset", () => testValidMimeType("text/plain;charset=utf-8"));
+
+    it("returns expected list of extensions for MIME type without charset", () => {
+        const type = MimeType.from("text/plain");
+        expect(type.extensions).toContain("txt");
     });
 
-    it("creates known MIME type with charset", () => {
-        testValidMimeType("text/plain;charset=utf-8");
+    it("returns expected list of extensions for MIME type without charset", () => {
+        const type = MimeType.from("text/plain;charset=utf-8");
+        expect(type.extensions).toContain("txt");
     });
+
+    it("returns expected main extension for TXT", () => testMainExtension("text/plain", "txt"));
+    it("returns expected main extension for PDF", () => testMainExtension("application/pdf", "pdf"));
+    it("returns expected main extension for JPG", () => testMainExtension("image/jpeg", "jpeg"));
+    it("returns expected main extension for PNG", () => testMainExtension("image/png", "png"));
+    it("returns expected main extension for GIF", () => testMainExtension("image/gif", "gif"));
 });
 
 function testValidMimeType(mimeType: string) {
     const type = MimeType.from(mimeType);
     expect(type.mimeType).toBe(mimeType);
+}
+
+function testMainExtension(mime: string, extension: string) {
+    const type = MimeType.from(mime);
+    expect(type.extensions[0]).toBe(extension);
 }
