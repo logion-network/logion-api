8412f07813f7f1d0d60c0a36a1597e5dd2ea34a9
Benoit Devos <benoit@logion.network>
2023-04-13 15:06:45 +0200

    feat: Use ValidAccount in LocRequest and LocClient.
    logion-network/logion-internal#851

--- packages/client/test/Loc.spec.ts
+++ packages/client/test/Loc.spec.ts
@@ -838,8 +838,7 @@ async function getVoidedCollectionLoc() {
 function expectDataToMatch(data: LocData, request: LocRequest) {
     expect(data.id.toString()).toBe(request.id.toString());
     expect(data.ownerAddress).toBe(request.ownerAddress);
-    expect(data.requesterAddress?.address).toBe(request.requesterAddress ? request.requesterAddress : undefined);
-    expect(data.requesterAddress?.type).toBe(request.requesterAddress ? "Polkadot" : undefined);
+    expect(data.requesterAddress).toEqual(request.requesterAddress);
     expect(data.requesterLocId?.toString()).toBe(request.requesterIdentityLoc ? request.requesterIdentityLoc : undefined);
     expect(data.description).toBe(request.description);
     expect(data.locType).toBe(request.locType);
