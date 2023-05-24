4ace9934dd6a72b5af385298b9b4119a34a4afa5
Benoit Devos <benoit@logion.network>
2023-05-24 11:34:55 +0200

    chore: suggestion from peer-review.
    logion-network/logion-internal#893

--- packages/node-api/integration/Fees.ts
+++ packages/node-api/integration/Fees.ts
@@ -1,4 +1,5 @@
 import { setup } from "./Util.js";
+import { Currency } from "../src/index.js";
 
 export async function storageFees() {
     const { api } = await setup();
@@ -9,15 +10,13 @@ export async function storageFees() {
     expect(fee).toBeGreaterThanOrEqual(0);
 }
 
-const ONE_LGNT = 1_000_000_000_000_000_000n;
-
 export async function legalFees() {
     const { api } = await setup();
     const identityLocFee = await api.fees.estimateLegalFee({ locType: "Identity"});
-    expect(identityLocFee).toEqual(160n * ONE_LGNT);
+    expect(identityLocFee).toEqual(Currency.toCanonicalAmount(Currency.nLgnt(160n)));
     const transactionLocFee = await api.fees.estimateLegalFee({ locType: "Transaction"});
-    expect(transactionLocFee).toEqual(2000n * ONE_LGNT);
+    expect(transactionLocFee).toEqual(Currency.toCanonicalAmount(Currency.nLgnt(2000n)));
     const collectionLocFee = await api.fees.estimateLegalFee({ locType: "Collection"});
-    expect(collectionLocFee).toEqual(2000n * ONE_LGNT);
+    expect(collectionLocFee).toEqual(Currency.toCanonicalAmount(Currency.nLgnt(2000n)));
 }
 
