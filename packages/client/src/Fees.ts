4463108b5497c3dbdd92112144251dd671f2d006
Gérard Dethier <gerard@logion.network>
2023-06-23 10:57:32 +0200

    chore: complete Fees view.

--- packages/client/src/Fees.ts
+++ packages/client/src/Fees.ts
@@ -1,8 +1,30 @@
+import { Fees as FeesClass } from "@logion/node-api";
+
+// The fields below must be kept in sync with FeesView in
+// https://github.com/logion-network/logion-backend-ts/blob/main/src/logion/controllers/components.ts
+
 export interface Fees {
     /** @description Inclusion fee */
     inclusion: string;
     /** @description File storage fee (if applicable) */
     storage?: string;
+    /** @description Legal fee (if applicable) */
+    legal?: string;
+    /** @description Certificate fee (if applicable) */
+    certificate?: string;
     /** @description Total fee (inclusion + storage) */
-    total: string;
+    total?: string;
+}
+
+export function toFeesClass(fees: Fees | undefined): FeesClass | undefined {
+    if(fees) {
+        return new FeesClass({
+            inclusionFee: BigInt(fees.inclusion),
+            storageFee: fees.storage ? BigInt(fees.storage) : undefined,
+            legalFee: fees.legal ? BigInt(fees.legal) : undefined,
+            certificateFee: fees.certificate ? BigInt(fees.certificate) : undefined,
+        });
+    } else {
+        return undefined;
+    }
 }
