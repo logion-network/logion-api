875714332620ddcf1b7233b0e16d1752a6ba0115
Benoit Devos <benoit@logion.network>
2024-04-17 12:34:26 +0200

    feat: add support for other account types; validate Bech32 addresses.
    logion-network/logion-internal#1216

--- packages/node-api/test/Types.spec.ts
+++ packages/node-api/test/Types.spec.ts
@@ -1,6 +1,58 @@
 import { ValidAccountId, AnyAccountId } from "../src/index.js";
 
-describe("ValidAccountId", () => {
+describe("ValidAccountId (Bech32)", () => {
+
+    const address1 = "erd1sqcp77ll8v8j6vgs5m0h2xxkz5wv26pfj2vyyls52cz6hdumkmjq0jr93a";
+    const address2 = "ERD1SQCP77LL8V8J6VGS5M0H2XXKZ5WV26PFJ2VYYLS52CZ6HDUMKMJQ0JR93A";
+
+    it("is valid and not case-sensitive", () => {
+        const account1 = ValidAccountId.bech32(address1);
+        const account2 = ValidAccountId.bech32(address2);
+
+        expect(account1.equals(account1)).toBeTrue();
+        expect(account1.equals(account2)).toBeTrue();
+
+        expect(account2.equals(account1)).toBeTrue();
+        expect(account2.equals(account2)).toBeTrue();
+    })
+
+    it("guesses from unknown", () => {
+        expect(ValidAccountId.fromUnknown(address1)?.type).toEqual("Bech32");
+        expect(ValidAccountId.fromUnknown(address2)?.type).toEqual("Bech32");
+    })
+})
+
+describe("ValidAccountId (Ethereum)", () => {
+
+    const address1 = "0x6ef154673a6379b2CDEDeD6aF1c0d705c3c8272a";
+    const address2 = "0x6ef154673a6379b2cdeded6af1c0d705c3c8272a";
+    const address3 = "0x6EF154673A6379B2CDEDED6AF1C0D705C3C8272A";
+
+    it("is valid and not case-sensitive", () => {
+        const account1 = ValidAccountId.ethereum(address1);
+        const account2 = ValidAccountId.ethereum(address2);
+        const account3 = ValidAccountId.ethereum(address3);
+
+        expect(account1.equals(account1)).toBeTrue();
+        expect(account1.equals(account2)).toBeTrue();
+        expect(account1.equals(account3)).toBeTrue();
+
+        expect(account2.equals(account1)).toBeTrue();
+        expect(account2.equals(account2)).toBeTrue();
+        expect(account2.equals(account3)).toBeTrue();
+
+        expect(account3.equals(account1)).toBeTrue();
+        expect(account3.equals(account2)).toBeTrue();
+        expect(account3.equals(account3)).toBeTrue();
+    })
+
+    it("guesses from unknown", () => {
+        expect(ValidAccountId.fromUnknown(address1)?.type).toEqual("Ethereum");
+        expect(ValidAccountId.fromUnknown(address2)?.type).toEqual("Ethereum");
+    })
+})
+
+describe("ValidAccountId (Polkadot)", () => {
 
     const address42 = "5HYf6QFkYpso8FdX9WALCmRTcga7YSmuFS5qqaJtFF7m4RPr";
     const address2021 = "vQxmTQGRHbTsBdDhVLqsksX7c44K8DjVokJUi8ZK58z88tDBx";
@@ -39,11 +91,26 @@ describe("ValidAccountId", () => {
 
     it("does not validate an invalid account", () => {
         expect(new AnyAccountId("BLA", "Polkadot").validate())
-            .toEqual("Wrong Polkadot address BLA: Error: Decoding BLA: Invalid decoded address length")
+            .toEqual("Wrong Polkadot address BLA: Invalid decoded address")
         expect(new AnyAccountId("INVALID", "Polkadot").validate())
-            .toEqual('Wrong Polkadot address INVALID: Error: Decoding INVALID: Invalid base58 character "I" (0x49) at index 0')
+            .toEqual('Wrong Polkadot address INVALID: Invalid base58 character "I" (0x49) at index 0')
         const invalid = "5HMzQmyDb8CU8ajJuvSrrqSH5LPHNRFS8888888888888888";
         expect(new AnyAccountId(invalid, "Polkadot").validate())
-            .toEqual("Wrong Polkadot address 5HMzQmyDb8CU8ajJuvSrrqSH5LPHNRFS8888888888888888: Error: Decoding 5HMzQmyDb8CU8ajJuvSrrqSH5LPHNRFS8888888888888888: Invalid decoded address checksum")
+            .toEqual("Wrong Polkadot address 5HMzQmyDb8CU8ajJuvSrrqSH5LPHNRFS8888888888888888: Invalid decoded address")
+    })
+
+    it("guesses from unknown", () => {
+        expect(ValidAccountId.fromUnknown(address42)?.type).toEqual("Polkadot");
+        expect(ValidAccountId.fromUnknown(address2021)?.type).toEqual("Polkadot");
     })
 })
+
+describe("ValidAccountId: fromUnknown()", () => {
+
+    it("returns undefined", () => {
+        expect(ValidAccountId.fromUnknown("BLA")).toBeUndefined();
+        expect(ValidAccountId.fromUnknown("INVALID")).toBeUndefined();
+        expect(ValidAccountId.fromUnknown("0x0000")).toBeUndefined();
+    });
+})
+
