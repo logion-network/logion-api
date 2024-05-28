2ad4db0a91805e2fe867dc49ce4f4862e30cc3de
Gérard Dethier <gerard@logion.network>
2024-05-28 09:51:12 +0200

    feat: enable secret value download.
    logion-network/logion-internal#1275

--- packages/client/src/SecretRecoveryClient.ts
+++ packages/client/src/SecretRecoveryClient.ts
@@ -14,6 +14,12 @@ export interface CreateSecretRecoveryRequest {
     userPostalAddress: PostalAddress;
 }
 
+export interface DownloadSecretRequest {
+    requesterIdentityLocId: UUID;
+    requestId: string;
+    challenge: string;
+}
+
 export class SecretRecoveryClient {
 
     constructor(args: { sharedState: SharedState }) {
@@ -22,17 +28,18 @@ export class SecretRecoveryClient {
 
     private sharedState: SharedState;
 
-    async createSecretRecoveryRequest(params: CreateSecretRecoveryRequest): Promise<void> {
+    async createSecretRecoveryRequest(params: CreateSecretRecoveryRequest): Promise<string> {
         const { secretName, challenge, userIdentity, userPostalAddress, requesterIdentityLocId } = params;
         const axios = await this.backend({ locId: requesterIdentityLocId });
         try {
-            await axios.post("/api/secret-recovery", {
+            const response = await axios.post("/api/secret-recovery", {
                 requesterIdentityLocId: requesterIdentityLocId.toString(),
                 secretName,
                 challenge,
                 userIdentity,
                 userPostalAddress,
             });
+            return response.data.id;
         } catch (e) {
             throw newBackendError(e);
         }
@@ -45,5 +52,18 @@ export class SecretRecoveryClient {
         });
         const legalOfficer = requireDefined(this.sharedState.legalOfficers.find(lo => lo.account.equals(loc.owner)));
         return legalOfficer.buildAxiosToNode();
+    }
+
+    async downloadSecret(params: DownloadSecretRequest): Promise<string> {
+        const { requesterIdentityLocId, requestId, challenge } = params;
+        const axios = await this.backend({ locId: requesterIdentityLocId });
+        try {
+            const response = await axios.put(`/api/secret-recovery/${ requestId.toString() }/download-secret`, {
+                challenge,
+            });
+            return response.data.value;
+        } catch (e) {
+            throw newBackendError(e);
+        }
     }
 }
--- packages/client/test/SecretRecovery.spec.ts
+++ packages/client/test/SecretRecovery.spec.ts
@@ -8,50 +8,88 @@ import { AxiosInstance, AxiosResponse } from "axios";
 describe("SecretRecovery", () => {
 
     it("requests a Secret recovery", async () => {
-
-        const identityLoc = buildLocAndRequest(ALICE.account, "CLOSED", "Identity");
-        const locId = new UUID(identityLoc.request.id);
-        const axios = new Mock<AxiosInstance>();
-
-        const config = buildTestConfig(testConfigFactory => {
-            testConfigFactory.setupDefaultNetworkState();
-            const axiosFactory = testConfigFactory.setupAxiosFactoryMock();
-            axiosFactory.setup(instance => instance.buildAxiosInstance(It.IsAny<string>(), It.IsAny()))
-                .returns(axios.object());
-            setupBackend(axios, locId);
-            const nodeApi = testConfigFactory.setupNodeApiMock(LOGION_CLIENT_CONFIG);
-            const directoryClient = testConfigFactory.setupDirectoryClientMock(LOGION_CLIENT_CONFIG);
-
-            directoryClient.setup(instance => instance.getLegalOfficers()).returns(Promise.resolve([
-                new LegalOfficerClass({
-                    legalOfficer: ALICE,
-                    axiosFactory: axiosFactory.object(),
-                })
-            ]));
-
-            nodeApi.setup(instance => instance.queries.getLegalOfficerCase(locId))
-                .returns(Promise.resolve(identityLoc.loc));
-        })
-
+        const { axios, config, locId } = configure();
+        setupBackendForCreation(axios, locId);
         const client = await LogionClient.create(config);
-        await client.secretRecovery.createSecretRecoveryRequest({
+
+        const id = await client.secretRecovery.createSecretRecoveryRequest({
             requesterIdentityLocId: locId,
             secretName: "secret-name",
-            challenge: "my-personal-challenge",
+            challenge: CHALLENGE,
             userIdentity: {} as UserIdentity,
             userPostalAddress: {} as PostalAddress,
         })
-        axios.verify(instance => instance.post("/api/secret-recovery", It.IsAny()), Times.Once());
+
+        expect(id).toBe(REQUEST_ID);
+    })
+
+    it("downloads a Secret value", async () => {
+        const { axios, config, locId } = configure();
+        setupBackendForDownload(axios, REQUEST_ID);
+        const client = await LogionClient.create(config);
+
+        const requestId = REQUEST_ID;
+        const value = await client.secretRecovery.downloadSecret({
+            requesterIdentityLocId: locId,
+            requestId,
+            challenge: CHALLENGE,
+        });
+
+        expect(value).toBe(SECRET_VALUE);
     })
 })
 
-function setupBackend(axios: Mock<AxiosInstance>, locId: UUID) {
+function configure() {
+    const identityLoc = buildLocAndRequest(ALICE.account, "CLOSED", "Identity");
+    const locId = new UUID(identityLoc.request.id);
+
+    const axios = new Mock<AxiosInstance>();
+    const config = buildTestConfig(testConfigFactory => {
+        testConfigFactory.setupDefaultNetworkState();
+        const axiosFactory = testConfigFactory.setupAxiosFactoryMock();
+        axiosFactory.setup(instance => instance.buildAxiosInstance(It.IsAny<string>(), It.IsAny()))
+            .returns(axios.object());
+        const nodeApi = testConfigFactory.setupNodeApiMock(LOGION_CLIENT_CONFIG);
+        const directoryClient = testConfigFactory.setupDirectoryClientMock(LOGION_CLIENT_CONFIG);
+
+        directoryClient.setup(instance => instance.getLegalOfficers()).returns(Promise.resolve([
+            new LegalOfficerClass({
+                legalOfficer: ALICE,
+                axiosFactory: axiosFactory.object(),
+            })
+        ]));
+
+        nodeApi.setup(instance => instance.queries.getLegalOfficerCase(locId))
+            .returns(Promise.resolve(identityLoc.loc));
+    });
+    return {
+        config,
+        locId,
+        axios,
+    };
+}
+
+function setupBackendForCreation(axios: Mock<AxiosInstance>, locId: UUID) {
     const response = new Mock<AxiosResponse<any>>();
+    response.setup(instance => instance.data).returns({ id: REQUEST_ID });
     axios.setup(instance => instance.post("/api/secret-recovery", It.Is<{ requesterIdentityLocId: string, secretName: string, challenge: string }>(body =>
         body.requesterIdentityLocId === locId.toString() &&
         body.secretName === "secret-name" &&
-        body.challenge === "my-personal-challenge",
+        body.challenge === CHALLENGE,
     )))
         .returns(Promise.resolve(response.object()))
 }
 
+const CHALLENGE = "my-personal-challenge";
+
+function setupBackendForDownload(axios: Mock<AxiosInstance>, requestId: string) {
+    const response = new Mock<AxiosResponse<any>>();
+    response.setup(instance => instance.data).returns({ value: SECRET_VALUE });
+    axios.setup(instance => instance.put(`/api/secret-recovery/${ requestId }/download-secret`, It.Is<{ challenge: string }>(body =>
+        body.challenge === "my-personal-challenge"
+    )))
+        .returns(Promise.resolve(response.object()))
+}
+
+const REQUEST_ID = "1f67ebed-0519-4bb3-80ab-60e4f5c6437e";
+const SECRET_VALUE = "Secret value";
